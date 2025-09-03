1cd6e6f80bca10f38f02942be88c4ad386a535ab
Pham Huu Loc <phamhuulocforwork@gmail.com>
2025-09-04 05:45:13 +0700

    feat: add "use client" directive to app-header and app-layout components; update default theme in providers to light

--- src/frontend/components/layout/app-layout.tsx
+++ src/frontend/components/layout/app-layout.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 
 import { AppHeader } from "./app-header";
--- src/frontend/components/shared/providers.tsx
+++ src/frontend/components/shared/providers.tsx
@@ -18,7 +18,7 @@ export function Providers({
   children,
   themeProps = {
     attribute: "class",
-    defaultTheme: "system",
+    defaultTheme: "light",
     enableSystem: true,
     disableTransitionOnChange: true,
   },
